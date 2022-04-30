74037260edf4c213725b63fb0287c499d079f430
Arpita1401 <rikumohanty2001@gmail.com>
2022-04-30 20:13:51 +0530

    initial commit

---
+++ src/components/LoginForm.js
@@ -0,0 +1,62 @@
+import { useState, useImperativeHandle } from "react";
+import axios from "axios";
+const LoginForm = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const authObject = {
+      "Project-ID": "7f2e1357-a6c7-495e-8740-21bad0e60073",
+      "User-Name": username,
+      "User-Secret": password,
+    };
+    try {
+      await axios.get("https://api.chatengine.io/chats", {
+        headers: authObject,
+      });
+      localStorage.setItem("username", username);
+      localStorage.setItem("password", password);
+
+      window.location.reload();
+    } catch (error) {
+      setError(" Oops,Try again! Incorrect Credentials");
+    }
+  };
+
+  return (
+    <div className="wrapper">
+      <div className="form">
+        <h1 className="title">Chat Application</h1>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            className="input"
+            placeholder="Username"
+            required
+          />
+          <input
+            type="text"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="input"
+            placeholder="Password"
+            required
+          />
+          <div align="center">
+            <button type="submit" className="button">
+              <span>Start Chatting</span>
+            </button>
+          </div>
+        </form>
+        <h2 className="error">{error}</h2>
+      </div>
+    </div>
+  );
+};
+
+export default LoginForm;
---
+++ src/components/MyMessage.js
@@ -0,0 +1,29 @@
+const MyMessage = ({ message }) => {
+  if (message?.attachments?.length > 0) {
+    return (
+      <img
+        src={message.attachments[0].file}
+        alt="message-attachment"
+        className="message-image"
+        style={{ float: "right" }}
+      />
+    );
+  }
+  message.text = message.text.replace("<p>", "");
+  message.text = message.text.replace("</p>", "");
+  return (
+    <div
+      className="message"
+      style={{
+        float: "right",
+        marginRight: "18px",
+        color: "white",
+        backgroundColor: "#3B2A50",
+      }}
+    >
+      {message.text}
+    </div>
+  );
+};
+
+export default MyMessage;
